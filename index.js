0a92d7c3343e79c710c7738041f8d53c169ab47e
Amber Robinson <ballewaer@gmail.com>
2019-12-09 15:34:20 -0800

    passed processSum

--- index.js
+++ index.js
@@ -67,8 +67,9 @@ function processLength(list, callback) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
+function processLastItem(stringList, callback) {
   /* CODE HERE */
+  return callback(stringList[stringList.length -1]);
 }
 
 /**
@@ -88,8 +89,9 @@ function processLastItem(/* CODE HERE */) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
+function processSum(numberList, callback) {
   /* CODE HERE */
+  return callback(numberList.reduce((amount, total) => amount + total, 0));
 }
 
 /**
