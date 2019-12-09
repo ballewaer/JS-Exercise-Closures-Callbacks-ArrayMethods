b38332c6c4fba22367c807a3e9ddaa5225d7a61c
Amber Robinson <ballewaer@gmail.com>
2019-12-09 15:55:36 -0800

    passed firstNamesAllCaps

--- index.js
+++ index.js
@@ -206,8 +206,14 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
+function firstNamesAllCaps(runners) {
   /* CODE HERE */
+  const firstNames = [];
+
+  runners.map(fn => {
+    firstNames.push(fn.first_name.toUpperCase());
+  });
+  return firstNames;
 }
 
 /**
