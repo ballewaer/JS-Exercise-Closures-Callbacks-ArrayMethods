3bd21693d6d26e2eb5a3599cf5417988df1f4c6c
Amber Robinson <ballewaer@gmail.com>
2019-12-09 16:14:57 -0800

    passed processDuplicateFree >STRETCH<

--- index.js
+++ index.js
@@ -163,8 +163,10 @@ function processContains(item, list, callback) {
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
 */
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
+function processDuplicateFree(list, callback) {
   /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+  let newArr = [...new Set (list)];
+  return callback(newArr);
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
